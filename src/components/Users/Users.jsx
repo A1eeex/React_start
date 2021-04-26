e23f8f5d22b22637fbd76650e782cb6083e72313
Alex <opetrovskyi423@gmail.com>
2021-04-26 23:11:05 +0300

    L-53_1.0

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,38 +2,41 @@ import React from 'react';
 import styles from "./Users.module.css"
 import * as axios from "axios";
 import userPhoto from "../../assec/img/user_.png"
-const Users = (props) => {
-  
-  if (props.users.length === 0) {
-   
-    axios.get("https://social-network.samuraijs.com/api/1.0/users")
-      .then(response =>{
-        props.setUsers(response.data.items)
-      })
-    
+
+class UsersC extends React.Component {
+  constructor(props) {
+    super(props)
+       axios.get("https://social-network.samuraijs.com/api/1.0/users")
+        .then(response => {
+          this.props.setUsers(response.data.items)
+        })
   }
-  return (
-    <div>
-      {
-        props.users.map(u => <div key={u.id}>
+  
+  render (){
+    return (
+      <div>
+        <button onClick={this.getUsers}>Get users</button>
+        {
+          this.props.users.map(u => <div key={u.id}>
         <span>
           <div>
-            <img className={styles.userAvatar} src={u.photos.samall !=null ?u.photos.samall:userPhoto } alt="avatar_photo"/>
+            <img className={styles.userAvatar} src={u.photos.samall != null ? u.photos.samall : userPhoto}
+                 alt="avatar_photo"/>
           </div>
           <div>
             {
               u.followed
                 ? <button onClick={() => {
-                  props.unfollow(u.id)
+                  this.props.unfollow(u.id)
                 }}>Unfollow</button>
                 : <button onClick={() => {
-                  props.follow(u.id)
+                  this.props.follow(u.id)
                 }}> Follow</button>
             }
           </div>
         </span>
           
-          <span>
+            <span>
             
             <span>
               <div>{u.name}</div>
@@ -44,13 +47,12 @@ const Users = (props) => {
               <div>{/*{u.location.country}*/}</div>
                <div>{/*{u.location.city}*/}</div>
             </span>
-            
           </span>
-        
-        </div>)
-      }
-    </div>
-  );
-};
+          </div>)
+        }
+      </div>
+    );
+  }
+}
 
-export default Users;
+export default UsersC;
