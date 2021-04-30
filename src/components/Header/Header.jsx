b7739a2636d2965c1927fb2a38d3566f49493d34
Alex <opetrovskyi423@gmail.com>
2021-04-30 21:49:09 +0300

    test

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import css_header_module from "./Header.module.css"
+import { NavLink } from "react-router-dom";
+
+
 const Header = () => {
-    return (
-        <header className={css_header_module.header}>
-            <img src="https://www.logodesign.net/images/nature-logo.png"></img>
-        </header>
-    )
+  return (
+    <header className={css_header_module.header}>
+      <img src="https://www.logodesign.net/images/nature-logo.png" alt="header_logo"> </img>
+      <div className={css_header_module.loginBlock}>
+        <NavLink to="/login"> Login</NavLink>
+      </div>
+    </header>
+  )
 }
 export default Header;
