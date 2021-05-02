2a5f6ed3313f1347bb02dbba9a534ef70ee1cb9e
Alex <opetrovskyi423@gmail.com>
2021-05-02 22:34:52 +0300

    L-63_0.1

---
+++ src/api/api.js
@@ -0,0 +1,22 @@
+import * as axios from "axios";
+
+const instance = axios.create({
+  baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+  withCredentials: true,
+  headers: {
+    "API-KEY": "af7d6b52-5311-47a1-ae15-7e3326a10cb7"
+  },
+  
+  
+})
+
+export const usersAPI = {
+  getUsers(currentPage = 1, pageSize = 10) {
+    return instance.get(`users?page=${currentPage}&count=${pageSize}`)
+      .then(response => {
+        return response.data;
+      });
+  }
+}
+
+
