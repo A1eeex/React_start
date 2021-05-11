a7349db31fc0de49a024d80fe5f4f1ae5e655143
Alex <opetrovskyi423@gmail.com>
2021-05-11 18:52:55 +0300

    L-74_1.0

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -19,8 +19,15 @@ class ProfileStatus extends React.Component {
   onStatusChange = (e) => {
    this.setState({
      status:   e.currentTarget.value
-  
    });
+  }
+  
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    if (prevProps.status !== this.props.status){
+     this.setState({
+       status: this.props.status
+     })
+    }
   }
   
   render() {
